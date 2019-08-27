68929686d378b75ad9c5caf7ab6cd7cf1633344c
Alvaro <alvort03@ucm.es>
2019-08-27 12:13:53 +0200

    %scripts%%scripts%%scripts%%scripts%X

--- commit.bat
+++ commit.bat
@@ -1,9 +1,14 @@
 @echo off
-
+rem Mejor rutas relativas a rutas absolutas, no desvelamos el arbol de directorios ~ Mas seguridad
 rem Guardar la carpeta Scripts
-echo Primero se va a actualizar la carpeta Scripts
-CALL "guardadoGIT.bat"
-echo !!Scripts guardados!!
+set /p scripts=Quieres actualizar la carpeta de Scripts? (Si/Yes/Else ~ No): 
+if "%scripts%"=="Si" or "%scripts%"=="S" or "%scripts%"=="Yes" or "%scripts%"=="Y" or "%scripts%"=="si" or "%scripts%"=="s" or "%scripts%"=="yes" or "%scripts%"=="y" (
+	echo "%scripts%"
+	echo "Si"
+	CALL "guardadoGIT.bat"
+	echo !!Scripts guardados!!
+)
+
 cd ../Codigo/
 dir
 
